14651461916ef9ec0479162f02a65e97e6e504b1
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2024-04-18 14:52:25 +0200

    updated docokerfile and app image

--- docker/python-app/main.py
+++ docker/python-app/main.py
@@ -1,6 +1,6 @@
 import calendar
 
-print('Test example app for Docker learning')
+print('Test example app for Docker learning [UPDATED: 2.0.0]')
 
 year = int(input('Please enter the year: '))
 month = int(input('Please enter the month: '))
