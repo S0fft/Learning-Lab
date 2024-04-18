64dca28957b40d1d83901f8af1ca201119817ea1
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2024-04-18 12:05:53 +0200

    add test app for docker learning

---
+++ docker/python-app/main.py
@@ -0,0 +1,9 @@
+import calendar
+
+print('Test example app for Docker learning')
+
+year = int(input('Please enter the year: '))
+month = int(input('Please enter the month: '))
+
+
+print(calendar.month(year, month))
