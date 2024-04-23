5064063581fd35244bc337324356349101a97711
Evgeniy Pelikh <pelikh123.evg@gmail.com>
2024-04-23 13:53:40 +0200

    add docker compose

---
+++ docker/docker_compose-app/app/main.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+
+from pymongo import MongoClient
+
+MONGO_URL = "mongodb://mongo:27017"
+client = MongoClient(MONGO_URL)
+db = client.admin
+dbs_list = db.command("listDatabases")
+pprint(dbs_list)
