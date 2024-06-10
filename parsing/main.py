292f40149d2f2d042cc4269a4e71e655e72c9f6b
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-06-10 16:13:44 +0200

    make func wrapper for parsing feauture. Create for yield

--- parsing/main.py
+++ parsing/main.py
@@ -22,15 +22,18 @@ def get_url():
             yield card_url
 
 
-for url in get_url():
-    response = requests.get(url)
-    sleep(3)
-    soup = BeautifulSoup(response.text, "lxml")
+def fetch_card_details():
 
-    data = soup.find("div", class_="my-8 w-full rounded border")
-    name = data.find("h3", class_="card-title").text
-    price = data.find("h4").text
-    text = data.find("p", class_="card-description").text
-    url_img = DOMAIN_NAME + data.find("img").get("src")
+    for url in get_url():
+        response = requests.get(url)
+        sleep(3)
+        soup = BeautifulSoup(response.text, "lxml")
 
-    print(name + '\n' + price + '\n' + text + '\n' + url_img + '\n  \n')
+        data = soup.find("div", class_="my-8 w-full rounded border")
+        name = data.find("h3", class_="card-title").text
+        price = data.find("h4").text
+        text = data.find("p", class_="card-description").text
+        url_img = DOMAIN_NAME + data.find("img").get("src")
+
+        yield name, price, text, url_img
+        # print(name + '\n' + price + '\n' + text + '\n' + url_img + '\n  \n')
