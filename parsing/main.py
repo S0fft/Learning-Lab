122d3f43238ecdd07d402021a60e6d5725ed7734
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-06-10 13:32:31 +0200

    optomize script with yield generator

--- parsing/main.py
+++ parsing/main.py
@@ -5,26 +5,26 @@ from bs4 import BeautifulSoup
 
 DOMAIN_NAME = "https://scrapingclub.com"
 
-list_card_url = []
+
+def get_url():
+    for page in range(1, 8):
+        url = f"https://scrapingclub.com/exercise/list_basic/?page={page}"
+
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, "lxml")
+
+        data = soup.find_all("div", class_="w-full rounded border")
+
+        for i in data:
+            card_url = DOMAIN_NAME + i.find("a").get("href")
+
+            yield card_url
 
 
-for page in range(1, 8):
-    url = f"https://scrapingclub.com/exercise/list_basic/?page={page}"
-
+for url in get_url():
     response = requests.get(url)
-
-    soup = BeautifulSoup(response.text, "lxml")
-
-    data = soup.find_all("div", class_="w-full rounded border")
-
-    for i in data:
-        card_url = DOMAIN_NAME + i.find("a").get("href")
-        list_card_url.append(card_url)
-
-
-for url in list_card_url:
-    response = requests.get(url)
-
+    sleep(3)
     soup = BeautifulSoup(response.text, "lxml")
 
     data = soup.find("div", class_="my-8 w-full rounded border")
@@ -33,4 +33,4 @@ for url in list_card_url:
     text = data.find("p", class_="card-description").text
     url_img = DOMAIN_NAME + data.find("img").get("src")
 
-    print(name + '\n' + price + '\n' + text + '\n' + url_img + '\n\n')
+    print(name + '\n' + price + '\n' + text + '\n' + url_img + '\n  \n')
