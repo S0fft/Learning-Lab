1a30885ca6ec6c399c3c4a69218350ffd9eb01fa
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-06-25 09:30:05 +0200

    add start command

--- tg_bot/run.py
+++ tg_bot/run.py
@@ -1,18 +1,30 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 from decouple import config
 
-bot = Bot(config("TOKEN"))
-dp = Dispatcher(bot)
+TOKEN = config("TOKEN")
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.message):
-    await bot.send_message(
-        message.chat.id,
-        'Hi! I\'m bot that will help you find some goods on <b><a href="https://rozetka.com.ua/ua/">Rozetka</a></b>',
-        parse_mode="html",
-        disable_web_page_preview=1
+def register_handlers(dp: Dispatcher):
+    dp.message.register(start, Command(commands=['start']))
+
+
+async def start(message: Message):
+    await message.answer(
+        'Hi! I\'m bot that will help you find some goods on <b><a href="https://rozetka.com.ua/ua/">Rozetka</a></b>!',
+        parse_mode="HTML",
+        disable_web_page_preview=True
     )
 
-executor.start_polling(dp)
+
+async def main():
+    register_handlers(dp)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
