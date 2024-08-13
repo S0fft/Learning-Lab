a55b99912606bb718be16b82ea649136ca61743c
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-08-13 17:19:42 +0200

    PNN Soft interview tasks

--- notes.py
+++ notes.py
@@ -1,3 +1,4 @@
+import copy
 import csv  # Работа с csv файлами
 import json  # Работа с JSON форматом
 import math  # Работа с матемтическми выражениями
@@ -1948,3 +1949,82 @@ try:
     do_sensitive_job()
 except Exception as e:
     print(e)
+
+
+class City:
+    def __init__(self, name='Kyiv'):
+        self.name = name
+
+
+class Inforamtion:
+    def __init__(self, created_date='12.08.24', city=None):
+        self.created_date = created_date
+        self.city = city if city else City()
+
+
+class User:
+    def __init__(self, name='Name', surname='Surname', age=20, info=None):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.info = info if info else Inforamtion()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+user_obj = User()
+print(user_obj.info.created_date)
+
+user_obj_2 = copy.copy(user_obj)
+user_obj_2.info.created_date = 'NEW'
+
+print(user_obj_2.info.created_date)
+print(user_obj.info.created_date)
+
+print(user_obj_2.info.city is user_obj.info.city)
+print(user_obj_2.info.city == user_obj.info.city)
+
+a: list[int] = [1, 2, 3]
+
+test_brackets = {
+    "()": True,
+    "{}": True,
+    "[]": True,
+    "[()]": True,
+    "[({})]": True,
+    "[({()})]": True,
+    "[({([])})]": True,
+    "[({([(){{()()(){}}}])})]": True,
+    "[]()[)[)": False,
+    "[]()()()[][)[)": False,
+    "()([][[[[}}}))": False,
+    "[": False,
+    "(": False,
+    "{": False,
+    "{{{": False,
+    "{{{{{]}{}{{}{{)))": False,
+    "[(])}": False,
+    "[(])()()}": False,
+}
+
+
+# ---------------------------------------------------
+
+
+def is_brackets_correct(input_brackets: str) -> bool:
+    result: list = []
+
+    if input_brackets.count('(') == input_brackets.count(')'):
+        result.append(1)
+
+    if input_brackets.count('[') == input_brackets.count(']'):
+        result.append(1)
+
+    if input_brackets.count('{') == input_brackets.count('}'):
+        result.append(1)
+
+    return True if len(result) == 3 else False
+
+
+print(is_brackets_correct('[({([])})]'))
