e01762b6e13c6d03136497b6aff5b2b04e09a3b0
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-08-19 13:12:52 +0200

    add new way for async run

--- async/asyncio_example.py
+++ async/asyncio_example.py
@@ -14,16 +14,26 @@ async def print3():
     print(3)
 
 
-async def main():
-    task1 = asyncio.create_task(print1())
-    task2 = asyncio.create_task(print2())
-    task3 = asyncio.create_task(print3())
+# async def main():
+#     task1 = asyncio.create_task(print1())
+#     task2 = asyncio.create_task(print2())
+#     task3 = asyncio.create_task(print3())
 
-    # await task1
-    # await task2
-    # await task3
+#     # await task1
+#     # await task2
+#     # await task3
+
+#     # OR
+
+#     await asyncio.gether(task1, task2, task3)
+
     # OR
-    await asyncio.gether(task1, task2, task3)
+
+async def main():
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(print1())
+        tg.create_task(print2())
+        tg.create_task(print3())
 
 
 asyncio.run(main())
