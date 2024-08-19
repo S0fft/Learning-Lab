dce6ea21b511d1b0fd7b005ff9323150c4fb56f0
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-08-19 11:52:30 +0200

    asyncio example

---
+++ async/asyncio_example.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+async def print1():
+    print(1)
+
+
+async def print2():
+    asyncio.sleep(10)
+    print(2)
+
+
+async def print3():
+    print(3)
+
+
+async def main():
+    print1()
+    print2()
+    print2()
+
+
+asyncio.run()
