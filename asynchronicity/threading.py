57c7409bac01becfc736b59dae9fc9b265faf744
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-09-13 11:20:14 +0200

    rename main folder

---
+++ asynchronicity/threading.py
@@ -0,0 +1,21 @@
+# import time
+# import tkinter as tk
+# from threading import Thread
+# from tkinter import ttk
+
+# root = tk.Tk()
+# root.geometry('300x300+150+150')
+
+
+# def thread_sleep():
+#     time.sleep(10)
+#     lab['text'] = int(lab['text']) + 10
+
+
+# def new_thread():
+#     t1 = Thread(target=thread_sleep)
+#     t1.start()
+
+
+# btn = ttk.Button(root, text='Run', command=new_thread)
+# btn.place(relx=0.5, rely=0.2, anchor=)
