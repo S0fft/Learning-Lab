57b59f2a0cfe062b8f5c1c5c51aa20f1e4cead52
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-11-14 13:10:58 +0100

    create Category model, add db_index for title in Book model

--- django_rest_framework_retrospective/books/models.py
+++ django_rest_framework_retrospective/books/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
 
     def __str__(self) -> str:
         return self.title
