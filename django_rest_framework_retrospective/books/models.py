716ce0ed6927de0cdf1e2f3497adb6d137578810
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-11-14 13:05:23 +0100

    create Book model

--- django_rest_framework_retrospective/books/models.py
+++ django_rest_framework_retrospective/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.title
