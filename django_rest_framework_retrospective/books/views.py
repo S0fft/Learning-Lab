98d8201db8b7446f794fb427c3af76142feaa449
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-11-14 14:11:38 +0100

    change path to serializer for book, add .

--- django_rest_framework_retrospective/books/views.py
+++ django_rest_framework_retrospective/books/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
-from serializers import BookSerializer
 
 from .models import Book
+from .serializers import BookSerializer
 
 
 class BookAPIView(generics.ListAPIView):
