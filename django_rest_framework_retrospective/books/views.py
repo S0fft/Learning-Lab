2ed6ac830d187e4348ab6ac132112447de91e24b
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-11-15 17:53:01 +0100

    change APIView, add get and post methods

--- django_rest_framework_retrospective/books/views.py
+++ django_rest_framework_retrospective/books/views.py
@@ -1,9 +1,27 @@
+from django.forms import model_to_dict
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Book
 from .serializers import BookSerializer
 
+# class BookAPIView(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
 
-class BookAPIView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+
+class BookAPIView(APIView):
+    def get(self, request):
+        lst = Book.objects.all().values()
+
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = Book.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            category_id=request.data['category_id']
+        )
+
+        return Response({'post': model_to_dict(post_new)})
