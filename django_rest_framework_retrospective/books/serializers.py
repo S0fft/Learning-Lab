0e0b8163445677cb424d314466e25446905b9ae2
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-11-18 16:28:53 +0100

    create new BookSerializer

--- django_rest_framework_retrospective/books/serializers.py
+++ django_rest_framework_retrospective/books/serializers.py
@@ -12,29 +12,37 @@ from .models import Book
 #         fields = ('title', 'category_id',)
 
 
-class BookModel:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
+# class BookModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
+
+# class BookSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=255)
+#     content = serializers.CharField()
+
+
+# def encode():
+#     model = BookModel('Test1', 'Content: Test1')
+#     model_sr = BookSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+
+
+# def decode():
+#     stream = io.BytesI0(b'{"title":"Test2", "content":"Test2"}')
+#     data = JSONParser().parse(stream)
+#     serializer = BookSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validate_data)
 
 class BookSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
+    category_id = serializers.IntegerField()
     content = serializers.CharField()
-
-
-def encode():
-    model = BookModel('Test1', 'Content: Test1')
-    model_sr = BookSerializer(model)
-    print(model_sr.data, type(model_sr.data), sep='\n')
-
-    json = JSONRenderer().render(model_sr.data)
-    print(json)
-
-
-def decode():
-    stream = io.BytesI0(b'{"title":"Test2", "content":"Test2"}')
-    data = JSONParser().parse(stream)
-    serializer = BookSerializer(data=data)
-    serializer.is_valid()
-    print(serializer.validate_data)
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
