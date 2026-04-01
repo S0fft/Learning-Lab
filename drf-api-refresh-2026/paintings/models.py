ed0d0e2ee1c9fbecce13e93e44d1935ff1741b35
Evgeniy Pelikh <eugene.p9g@gmail.com>
2026-04-01 17:58:06 +0200

    create Painting and Category models

--- drf-api-refresh-2026/paintings/models.py
+++ drf-api-refresh-2026/paintings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Painting(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    category_name = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title or f'Unnamed: {self.time_create}'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.title or f'Unnamed category'
