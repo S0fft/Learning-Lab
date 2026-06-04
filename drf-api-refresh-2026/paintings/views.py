c9555ab3a9917693ff298cc60ebc3cadc267c2c3
Evgeniy Pelikh <eugene.p9g@gmail.com>
2026-06-04 12:15:35 +0200

    create get and post methods

--- drf-api-refresh-2026/paintings/views.py
+++ drf-api-refresh-2026/paintings/views.py
@@ -1,10 +1,27 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Painting
 from .serializers import PaintingSerializer
 
+# class PaintingAPIView(generics.ListAPIView):
+#     queryset = Painting.objects.all()
+#     serializer_class = PaintingSerializer
 
-class PaintingAPIView(generics.ListAPIView):
-    queryset = Painting.objects.all()
-    serializer_class = PaintingSerializer
+
+class PaintingAPIView(APIView):
+    def get(self, request):
+        lst = Painting.objects.all().values()
+
+        return Response({'Painting': list(lst)})
+
+    def post(self, request):
+        new_post = Painting.objects.create(
+            title=request.data['title'],
+            description=request.data['description'],
+            category_name_id=request.data['category_name']
+        )
+        return Response({'post': model_to_dict(new_post)})
